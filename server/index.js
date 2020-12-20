85b8604dc8eb1ece9af58b4f42e527d797aa6eba
indo <indogusmasarungsamudra@gmail.com>
2020-12-20 22:42:53 +0700

    add

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';  
+import cors from 'cors';
+import postRoutes from './routes/post.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({limit: "30mb",extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}));
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+
+const CONNECTION_URL = 'vsbdcs';
+
+
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
+
+
